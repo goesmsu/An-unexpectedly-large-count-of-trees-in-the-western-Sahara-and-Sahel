6cdad0c8f51c1a6c4953ddecf82455007642fbe6
Nikit Parakh <parakhni@msu.edu>
2022-04-25 14:57:57 -0400

    Modified code to have separate user folders - analysis

--- notebooks/configTemplate/Preprocessing.py
+++ notebooks/configTemplate/Preprocessing.py
@@ -9,29 +9,22 @@ class Configuration:
     Copy the configTemplate folder and define the paths to input and output data. Variables such as raw_ndvi_image_prefix may also need to be corrected if you are use a different source.
     '''
     def __init__(self):
-        self.training_base_dir = '../data'
-        self.training_area_fn = 'India_polygons_v3.shp'
-        self.training_polygon_fn = 'india_trees_slice.shp'
+        self.training_base_dir = '../data'       # Do Not Change
+        self.training_area_fn = 'polygons.shp'   # Do Not Change
+        self.training_polygon_fn = 'trees.shp'   # Do Not Change
 
         # For reading the VHR images
         self.bands = [0]
-        self.raw_image_base_dir = '../data'
-        self.raw_image_file_type = '.tif'
-        self.raw_ndvi_image_prefix = 'wv03_20190407054002_1040010048338400_19apr07054002-m1bs-503158889080_01_p006_utm43n_b357_pan_ndvi'
-        self.raw_pan_image_prefix = 'wv03_20190407054002_1040010048338400_19apr07054002-m1bs-503158889080_01_p006_utm43n_b357_pan_sub'
+        self.raw_image_base_dir = '../data'      # Do Not Change
+        self.raw_image_file_type = '.tif'        # Do Not Change
+        self.raw_ndvi_image_prefix = 'ndvi_'     # Do Not Change
+        self.raw_pan_image_prefix = 'pan_'       # Do Not Change
 
         # For writing the extracted images and their corresponding annotations and boundary file
-        self.path_to_write = '../results'
+        self.path_to_write = '../results'        # Do Not Change
         self.show_boundaries_during_processing = False
         self.extracted_file_type = '.png'
-        self.extracted_ndvi_filename = 'ndvi'
-        self.extracted_pan_filename = 'pan'
-        self.extracted_annotation_filename = 'annotation'
-        self.extracted_boundary_filename = 'boundary'
-        
-
-        # Path to write should be a valid directory
-#         assert os.path.exists(self.path_to_write)
-
-#         if not len(os.listdir(self.path_to_write)) == 0:
-#             print('Warning: path_to_write is not empty! The old files in the directory may not be overwritten!!')
+        self.extracted_ndvi_filename = 'ndvi'             # Do Not Change
+        self.extracted_pan_filename = 'pan'               # Do Not Change
+        self.extracted_annotation_filename = 'annotation' # Do Not Change
+        self.extracted_boundary_filename = 'boundary'     # Do Not Change
--- notebooks/configTemplate/UNetTraining.py
+++ notebooks/configTemplate/UNetTraining.py
@@ -6,12 +6,12 @@ class Configuration:
         # Initialize the data related variables used in the notebook
         # For reading the ndvi, pan and annotated images generated in the Preprocessing step.
         # In most cases, they will take the same value as in the config/Preprocessing.py
-        self.base_dir = '../results'
-        self.image_type = '.png'
-        self.ndvi_fn = 'ndvi'
-        self.pan_fn = 'pan'
-        self.annotation_fn = 'annotation'
-        self.weight_fn = 'boundary'
+        self.base_dir = '../results'        # Do Not Change
+        self.image_type = '.png'            # Same as self.extracted_file_type in Preprocessing.py
+        self.ndvi_fn = 'ndvi'               # Do Not Change
+        self.pan_fn = 'pan'                 # Do Not Change
+        self.annotation_fn = 'annotation'   # Do Not Change
+        self.weight_fn = 'boundary'         # Do Not Change
         
         # Patch generation; from the training areas (extracted in the last notebook), we generate fixed size patches.
         # random: a random training area is selected and a patch in extracted from a random location inside that training area. Uses a lazy stratergy i.e. batch of patches are extracted on demand.
@@ -30,23 +30,23 @@ class Configuration:
 
         
         # The split of training areas into training, validation and testing set, is cached in patch_dir.
-        self.patch_dir = './patches{}'.format(self.patch_size[0])
-        self.frames_json = os.path.join(self.patch_dir,'frames_list.json')
+        self.patch_dir = './patches{}'.format(self.patch_size[0])               # Do Not Change
+        self.frames_json = os.path.join(self.patch_dir,'frames_list.json')      # Do Not Change
 
 
         # Shape of the input data, height*width*channel; Here channels are NVDI and Pan
-        self.input_shape = (256,256,2)
-        self.input_image_channel = [0,1]
-        self.input_label_channel = [2]
-        self.input_weight_channel = [3]
+        self.input_shape = (256,256,2)         # Do Not Change
+        self.input_image_channel = [0,1]       # Do Not Change
+        self.input_label_channel = [2]         # Do Not Change
+        self.input_weight_channel = [3]        # Do Not Change
 
         # CNN model related variables used in the notebook
-        self.BATCH_SIZE = 1
+        self.BATCH_SIZE = 1                        # Do Not Change
         self.NB_EPOCHS = 100
 
         # number of validation images to use
         self.VALID_IMG_COUNT = 192
         # maximum number of steps_per_epoch in training
         self.MAX_TRAIN_STEPS = 1000
-        self.model_path = './saved_models/UNet/'
+        self.model_path = './saved_models/UNet/'   # Do Not Change
 
